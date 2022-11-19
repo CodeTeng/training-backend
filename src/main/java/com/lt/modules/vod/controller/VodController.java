ac091a348cd645d5d08b3eb3520f250df2470e4a
teng <teng2002823@outlook.com>
2022-11-20 00:34:49 +0800

    add vod module

---
+++ src/main/java/com/lt/modules/vod/controller/VodController.java
@@ -0,0 +1,14 @@
+package com.lt.modules.vod.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: 视频上传
+ * @author: ~Teng~
+ * @date: 2022/11/19 11:56
+ */
+@RestController
+@RequestMapping("/sys/vod")
+public class VodController {
+}
