212bea987a60a725623ae93776151297986dcab9
teng <teng2002823@outlook.com>
2022-11-25 18:13:36 +0800

    add ExamConstant.java

---
+++ src/main/java/com/lt/constant/ExamConstant.java
@@ -0,0 +1,13 @@
+package com.lt.constant;
+
+/**
+ * @description: 考试相关常量
+ * @author: ~Teng~
+ * @date: 2022/11/22 9:21
+ */
+public interface ExamConstant {
+    /**
+     * 相关题目解析分隔符
+     */
+    String SEPARATOR = "list_separator";
+}
